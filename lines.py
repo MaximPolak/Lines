16c9b646d1baec02e12457ed8990c344f7fffd35
Maxim Polák <polakmaxi@gmail.com>
2021-05-12 16:55:18 +0200

    Tisknutí daných řádků

--- lines.py
+++ lines.py
@@ -2,3 +2,8 @@
 import sys
 
 cisla_radku = sys.argv[1:]
+
+for i, radek in enumerate(sys.stdin):
+    cislo_aktualniho_radku = i + 1
+    if str(cislo_aktualniho_radku) in cisla_radku:
+        print(radek, end="")

0dbd5df93d40649748c56b7ecc5273f7840fa70c
Maxim Polák <polakmaxi@gmail.com>
2021-05-13 16:31:25 +0200

    Zmatení gitu

--- lines.py
+++ lines.py
@@ -5,8 +5,8 @@ cisla_radku = sys.argv[1:]
 
 seznam_radku_na_pretisknuti = set()
 for cislo in cisla_radku:
-    if "-" in cislo:
-        rozsah = cislo.split("-")
+    rozsah = cislo.split("-")
+    if len(rozsah) > 1:
         seznam_radku_na_pretisknuti.update(range(int(rozsah[0]), int(rozsah[1]) + 1, 1 if len(rozsah) == 2 else int(rozsah[2])))
     else:
         seznam_radku_na_pretisknuti.add(int(cislo))
@@ -14,7 +14,7 @@ for cislo in cisla_radku:
 if seznam_radku_na_pretisknuti:
     for i, radek in enumerate(sys.stdin, start=1):
         if i in seznam_radku_na_pretisknuti:
-            print(radek, end="")
+            sys.stdout.write(radek)
             seznam_radku_na_pretisknuti.remove(i)
         if not seznam_radku_na_pretisknuti:
             break
